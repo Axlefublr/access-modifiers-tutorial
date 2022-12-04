ee9f618fb58887d42fbc1c22d968e7d37c2aeb78
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2022-12-04 16:52:29 +0800

    Added MainDerived

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using Other;
+
+internal class Program
 {
    private static void Main(string[] args)
    {
       Console.WriteLine("Hello, World!");
+   }
+}
+
+internal class MainDerived : OtherClass {
+   private static void MainDerivedTesting() {
+      
    }
 }
